07b86328587e6909f0186d62c2ff48ceb1e8601c
damianstrag <damian_jan@onet.eu>
2022-10-25 10:16:37 +0200

    Project Start!!

---
+++ src/main/java/pl/books/WorldOfBooksApplication.java
@@ -0,0 +1,13 @@
+package pl.books;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldOfBooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorldOfBooksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/books/WorldOfBooksApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.books;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorldOfBooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
